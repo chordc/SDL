784fcc6a193db539c7f71d4842424719ae6a0545
chordc <90882617+chordc@users.noreply.github.com>
2023-04-18 17:29:37 +0800

    Add files via upload

---
+++ learnppo.py
@@ -0,0 +1,160 @@
+import torch
+import numpy as np
+import time
+from physo.learn import ppo
+# Internal imports
+def learner_ppo ( model,
+             n_epochs,
+             batch_reseter,
+             risk_factor,
+             verbose = True,
+             stop_reward = 1.,
+             stop_after_n_epochs = 50,
+             run_logger     = None,
+             run_visualiser = None,
+            ):
+    """
+    Trains model to generate symbolic programs satisfying a reward by reinforcing on best candidates at each epoch.
+    Parameters
+    ----------
+    model : torch.nn.Module
+        Differentiable RNN cell.
+    optimizer : torch.optim
+        Optimizer to use.
+    n_epochs : int
+        Number of epochs.
+    batch_reseter : callable
+        Function returning a new empty physym.batch.Batch.
+    risk_factor : float
+        Fraction between 0 and 1 of elite programs to reinforce on.
+    gamma_decay : float
+        Weight of power law to use along program length: gamma_decay**t where t is the step in the sequence in the loss
+        function (gamma_decay < 1 gives more important to first tokens and gamma_decay > 1 gives more weight to last
+        tokens).
+    entropy_weight : float
+        Weight to give to entropy part of the loss.
+    verbose : int, optional
+        If verbose = False or 0, print nothing, if True or 1, prints learning time, if > 1 print epochs progression.
+    stop_reward : float, optional
+        Early stops if stop_reward is reached by a program (= 1 by default), use stop_reward = (1-1e-5) when using free
+        constants.
+    stop_after_n_epochs : int, optional
+        Number of additional epochs to do after early stop condition is reached.
+    run_logger : object or None, optional
+        Custom run logger to use having a run_logger.log method taking as args (epoch, batch, model, rewards, keep,
+        notkept, loss_val).
+    run_visualiser : object or None, optional
+        Custom run visualiser to use having a run_visualiser.visualise method taking as args (run_logger, batch).
+    Returns
+    -------
+    hall_of_fame_R, hall_of_fame : list of float, list of physym.program.Program
+        hall_of_fame : history of overall best programs found.
+        hall_of_fame_R : Corresponding reward values.
+        Use hall_of_fame[-1] to access best model found.
+    """
+    t000 = time.perf_counter()
+
+    # Basic logs
+    overall_max_R_history = []
+    hall_of_fame          = []
+
+    for epoch in range (n_epochs):
+
+        if verbose>1: print("Epoch %i/%i"%(epoch, n_epochs))
+
+        # -------------------------------------------------
+        # --------------------- INIT  ---------------------
+        # -------------------------------------------------
+
+        # Reset new batch (embedding reset)
+        ppo1=ppo.PPO(acmodel=model,batch_reseter=batch_reseter)
+        # Number of elite candidates to keep
+        n_keep = int(risk_factor*ppo1.batch_size)
+
+        # -------------------------------------------------
+        # -------------------- RNN RUN  -------------------
+        # -------------------------------------------------
+
+        # RNN run
+       
+        ppo1.collect_experiences()
+
+        # -------------------------------------------------
+        # ---------------- BEST CANDIDATES ----------------
+        # -------------------------------------------------
+
+        
+        # ----------------- Train batch : black box part (NUMPY) -----------------
+        # Loss
+        loss_val = ppo1.update_parameters(n_keep)
+
+        # -------------------------------------------------
+        # ---------------- BACKPROPAGATION ----------------
+        # -------------------------------------------------
+        # No need to do backpropagation if model is lobotomized (ie. is just a random number generator).
+        if model.is_lobotomized:
+            pass
+        else:
+            ppo1.optimizer.zero_grad()
+            loss_val  .backward()
+            grad_norm = sum(p.grad.data.norm(2).item() ** 2 for p in ppo1.acmodel.parameters()) ** 0.5
+            torch.nn.utils.clip_grad_norm_(ppo1.acmodel.parameters(), ppo1.max_grad_norm)
+            ppo1.optimizer .step()
+
+        # -------------------------------------------------
+        # ----------------- LOGGING VALUES ----------------
+        # -------------------------------------------------
+
+        # Basic logging (necessary for early stopper)
+        if epoch == 0:
+            overall_max_R_history       = [ppo1.R.max()]
+            hall_of_fame                = [ppo1.batch.programs.get_prog(ppo1.R.argmax())]
+        if epoch> 0:
+            if ppo1.R.max() > np.max(overall_max_R_history):
+                overall_max_R_history.append(ppo1.R.max())
+                hall_of_fame.append(ppo1.batch.programs.get_prog(ppo1.R.argmax()))
+            else:
+                overall_max_R_history.append(overall_max_R_history[-1])
+
+        # Custom logging
+        if run_logger is not None:
+            run_logger.log(epoch    = epoch,
+                           batch    = ppo1.batch,
+                           model    = model,
+                           rewards  = ppo1.R,
+                           keep     = ppo1.keep,
+                           notkept  = ppo1.notkept,
+                           loss_val = loss_val)
+
+        # -------------------------------------------------
+        # ----------------- VISUALISATION -----------------
+        # -------------------------------------------------
+
+        # Custom visualisation
+        if run_visualiser is not None:
+            run_visualiser.visualise(run_logger = run_logger, batch =ppo1.batch)
+
+        # -------------------------------------------------
+        # -------------------- STOPPER --------------------
+        # -------------------------------------------------
+        early_stop_reward_eps = 2*np.finfo(np.float32).eps
+
+        # If above stop_reward (+/- eps) stop after [stop_after_n_epochs] epochs.
+        if (stop_reward - overall_max_R_history[-1]) <= early_stop_reward_eps:
+            if stop_after_n_epochs == 0:
+                try:
+                    run_visualiser.save_visualisation()
+                    run_visualiser.save_data()
+                    run_visualiser.save_pareto_data()
+                    run_visualiser.save_pareto_fig()
+                except:
+                    print("Unable to save last plots and data before stopping.")
+                break
+            stop_after_n_epochs -= 1
+
+    t111 = time.perf_counter()
+    if verbose:
+        print("  -> Time = %f s"%(t111-t000))
+
+    hall_of_fame_R = np.array(overall_max_R_history)
+    return hall_of_fame_R, hall_of_fame
---
+++ ppo.py
@@ -0,0 +1,340 @@
+import torch
+import numpy as np
+import time
+
+
+class PPO():
+    """The Proximal Policy Optimization algorithm
+    ([Schulman et al., 2015](https://arxiv.org/abs/1707.06347)).
+    refer to github link: https://github.com/lcswillems/torch-ac/blob/master/torch_ac/algos/ppo.py"""
+
+    def __init__(self, acmodel, 
+                 batch_reseter,
+                 gamma_decay=0.7,
+                 verbose=True,
+                 device=None, 
+                 discount=0.99,
+                 stop_reward=1., 
+                 lr=0.001, 
+                 gae_lambda=0.95, 
+                 entropy_coef=0.01,
+                 value_loss_coef=0.5, 
+                 max_grad_norm=0.5, 
+                 adam_eps=1e-8,
+                 clip_eps=0.2,
+                 recurrence=4,
+                 preprocess_obss=None, 
+                 reshape_reward=None,
+                 run_logger=None,
+                 run_visualizer=None):
+        """
+        Parameters:
+        ----------
+        acmodel : torch.Module
+            the model
+        num_frames_per_proc : int
+            the number of frames collected by every process for an update
+        discount : float
+            the discount for future rewards
+        lr : float
+            the learning rate for optimizers
+        gae_lambda : float
+            the lambda coefficient in the GAE formula
+            ([Schulman et al., 2015](https://arxiv.org/abs/1506.02438))
+        entropy_coef : float
+            the weight of the entropy cost in the final objective
+        value_loss_coef : float
+            the weight of the value loss in the final objective
+        max_grad_norm : float
+            gradient will be clipped to be at most this value
+        recurrence : int
+            the number of steps the gradient is propagated back in time
+        preprocess_obss : function
+            a function that takes observations returned by the environment
+            and converts them into the format that the model can handle
+        reshape_reward : function
+            a function that shapes the reward, takes an
+            (observation, action, reward, done) tuple as an input
+        """
+
+        self.acmodel = acmodel
+        self.device = device
+        self.discount = discount
+        self.lr = lr
+        self.gae_lambda = gae_lambda
+        self.entropy_coef = entropy_coef
+        self.value_loss_coef = value_loss_coef
+        self.gamma_decay = gamma_decay
+        self.max_grad_norm = max_grad_norm
+        self.adam_eps = adam_eps
+        self.clip_eps = clip_eps
+        self.preprocess_obss = preprocess_obss
+        self.reshape_reward = reshape_reward
+        self.batch_reseter = batch_reseter
+        self.run_logger = run_logger
+        self.run_visualizer = run_visualizer
+        self.verbose = verbose
+        self.stop_reward = stop_reward
+        self.optimizer=torch.optim.Adam(self.acmodel.parameters(),lr=self.lr,eps=self.adam_eps)
+        self.logits=[]
+        self.actions=[]
+        self.values=[]
+        #self.log_probs=[]
+        self.rewards=[]
+        self.advantages=[]
+        self.state=[]
+        self.observations=[]
+        self.recurrence=recurrence
+        self.batch=self.batch_reseter()
+        self.batch_size=self.batch.batch_size
+        self.max_time_step=self.batch.max_time_step
+
+        self.states= self.acmodel.get_zeros_initial_state(self.batch_size)
+        # Optimizer reset
+        #self.optimizer.zero_grad()
+
+    
+
+    def collect_experiences(self):
+        #exps : DictList
+
+        for i in range(self.max_time_step):
+            #observations
+            observations = torch.tensor(self.batch.get_obs().astype(np.float32), requires_grad=False,)
+            
+            #do one agent-environment interaction
+            res,value, states= self.acmodel(input_tensor=observations,states=self.states)
+
+            #self.states=memory
+
+            #save memories
+            self.state.append(self.states)
+            self.states=states
+
+            #save observations
+            self.observations.append(observations)
+
+            #raw pro distribution
+            outlogit=res
+
+            #prior
+            prior_array=self.batch.prior().astype(np.float32)
+
+            #0 protection
+            eplison=0
+            prior_array[prior_array==0]=eplison
+
+            #to log
+            prior=torch.tensor(prior_array,requires_grad=False)
+            logprior=torch.log(prior)
+
+            #sample
+            logit=outlogit+logprior
+            action=torch.multinomial(torch.exp(logit),num_samples=1)[:,0]
+
+            #log_prob=dist.log_prob(action)
+
+            #save actions
+            self.actions.append(action)
+            self.logits.append(logit)
+            #self.log_probs.append(log_prob)
+
+            #informing embedding of new action
+            self.batch.programs.append(action.detach().cpu().numpy())
+
+            #compute reward
+            if i==self.max_time_step-1:
+                self.logits=torch.stack(self.logits,dim=0)
+                self.actions=torch.stack(self.actions,dim=0)
+
+                self.actions_array=self.actions.detach().cpu().numpy()
+                          
+                self.R=self.batch.get_rewards()
+                reward=self.R
+                reward=torch.from_numpy(reward).float()
+            else:
+                reward=torch.zeros(action.shape[0],dtype=torch.float32)
+            
+            #update values
+            self.rewards.append(reward)
+            self.values.append(value)
+            advantage=torch.zeros(action.shape[0],dtype=torch.float32)
+            self.advantages.append(advantage)
+
+        #add advantage
+
+        for i in reversed(range(self.max_time_step)):
+            next_value=self.values[i+1] if i<self.max_time_step-1 else 0
+            next_advantage=self.advantages[i+1] if i<self.max_time_step-1 else 0
+            delta=self.rewards[i]+self.discount*next_value-self.values[i]
+            self.advantages[i]=delta+self.discount*self.gae_lambda*next_advantage
+            
+        #define exp
+        return 0
+    def update_parameters(self,n_keep):
+        #only keep best batch(candidates)
+        keep=self.R.argsort()[::-1][0:n_keep].copy()
+        self.keep=keep
+        notkept=self.R.argsort()[::-1][n_keep:].copy()
+        self.notkept=notkept
+        #self.keep=keep
+
+        #Elite candidates
+        self.actions_array_train=self.actions_array[:,keep]
+        ideal_probs_array_train=np.eye(self.batch.n_choices)[self.actions_array_train]
+
+        #Elite candidates rewards
+        R_train=torch.tensor(self.R[keep],requires_grad=False)  
+        R_lim=R_train.min()
+
+        #Elite candidates as one hot in torch
+        ideal_probs_train=torch.tensor(ideal_probs_array_train.astype(np.float32),requires_grad=False)
+
+        #Elite candidates advantages
+        advantages_train=torch.stack(self.advantages,dim=0)[:,keep]
+
+        #Elite candidates values
+        values_train=torch.stack(self.values,dim=0)[:,keep]
+
+        #Elite candidates log_probs
+        #log_probs_train=torch.stack(self.log_probs,dim=0)[:,keep]
+
+        #Elite candidates memories
+        #memories_train=torch.stack(self.memories,dim=0)[:,:,:,keep]
+       
+        #Elite candidates observations
+        #observations_train=torch.stack(self.observations,dim=0)[:,keep]
+
+        #Elite candidates logits
+        logits_train=self.logits[:,keep]
+
+        #compute new dist, value, memory
+        new_logits_trains=[]
+        new_values_trains=[]
+        #new_log_probs=[]
+        #entropys=[]
+
+        #reset new batch
+        self.batch=self.batch_reseter()
+
+        for i in range(self.max_time_step):
+            new_res,new_value, _ = self.acmodel(input_tensor=self.observations[i],states=self.state[i])
+            #prior
+            prior_array=self.batch.prior().astype(np.float32)
+
+            #new_log_prob=new_dist.log_prob(self.actions[i])
+            #entropy=new_dist.entropy()
+
+            #0 protection
+            eplison=0
+            prior_array[prior_array==0]=eplison
+
+            #to log
+            prior=torch.tensor(prior_array,requires_grad=False)
+            logprior=torch.log(prior)
+
+            #sample
+            logit=new_res+logprior
+            new_logits_trains.append(logit)
+            new_values_trains.append(new_value)
+            #new_log_probs.append(new_log_prob)
+            #entropys.append(entropy)
+        
+        new_logits_trains=torch.stack(new_logits_trains,dim=0)[:,keep]
+        new_values_trains=torch.stack(new_values_trains,dim=0)[:,keep]
+        #new_log_probs=torch.stack(new_log_probs,dim=0)[:,keep]
+        #entropys=torch.stack(entropys,dim=0)[:,keep]
+        #sum_entropys=entropys.sum(dim=0)
+        #sum entropy
+        #entropy=entropys.sum()
+        #Compute Loss
+        lengths=self.batch.programs.n_lengths[keep]
+        baseline=R_lim
+
+        loss_val=self.loss_func(logits_train=logits_train,
+                                new_logits_trains=new_logits_trains,
+                                new_values_trains=new_values_trains,
+                                #new_log_probs=new_log_probs,
+                                values_train=values_train,
+                                #log_probs_train=log_probs_train,
+                                advantages_train=advantages_train,
+                                ideal_probs_train=ideal_probs_train,
+                                lengths=lengths,
+                                #entropy=sum_entropys,
+                                baseline=baseline,)
+        return loss_val
+
+    
+    def safe_cross_entropy(self,p, logq, dim=-1):
+        safe_logq = torch.where(p == 0, torch.ones_like(logq), logq)#
+        return -torch.sum(p * safe_logq, dim=dim)
+
+    def loss_func(self,
+                  logits_train,
+                  new_logits_trains,
+                  new_values_trains,
+                  #new_log_probs,
+                  values_train,
+                  #log_probs_train,
+                  advantages_train,
+                  ideal_probs_train,
+                  lengths,
+                  #entropy,
+                  baseline,
+                  ):
+        # Getting shape
+        (max_time_step, n_train, n_choices,) = ideal_probs_train.shape
+
+        #Length mask
+        mask_length_np = np.tile(np.arange(0, max_time_step), (n_train, 1)  # (n_train, max_time_step,)
+                             ).astype(int) < np.tile(lengths, (max_time_step, 1)).transpose()
+        mask_length_np = mask_length_np.transpose().astype(float)  # (max_time_step, n_train,)
+        mask_length = torch.tensor(mask_length_np, requires_grad=False)  # (max_time_step, n_train,)
+
+        #Entropy mask
+        entropy_gamma_decay = np.array([self.gamma_decay ** t for t in range(self.max_time_step)])  # (max_time_step,)
+        entropy_decay_mask_np = np.tile(entropy_gamma_decay,
+                                    (n_train, 1)).transpose() * mask_length_np  # (max_time_step, n_train,)
+        entropy_decay_mask = torch.tensor(entropy_decay_mask_np, requires_grad=False)  # (max_time_step, n_train,)
+
+        #Normaizing over action dim probs and logprobs
+        probs=torch.nn.functional.softmax(logits_train,dim=2)
+       
+        log_probs=torch.log_softmax(logits_train,dim=2)
+        next_log_probs=torch.log_softmax(new_logits_trains,dim=2)
+        neglogp_per_step=self.safe_cross_entropy(ideal_probs_train,log_probs,dim=2) #(max_time_step, n_train,)
+        next_neglogp_per_step=self.safe_cross_entropy(ideal_probs_train,next_log_probs,dim=2) #(max_time_step, n_train,)
+
+        neglogp=torch.sum(neglogp_per_step*mask_length,dim=0)
+        next_neglogp=torch.sum(next_neglogp_per_step*mask_length,dim=0)
+
+        #Sum over action dim
+        entropy_per_step=self.safe_cross_entropy(probs,log_probs,dim=2) #(max_time_step, n_train,)
+        entropy=entropy_per_step.sum(dim=0).mean()
+        #new_entropy_per_step=self.safe_cross_entropy(probs,next_log_probs,dim=2)
+
+        # Sum over sequence dim, mean over batch dim
+
+        #ratio=torch.exp(next_log_probs-log_probs)
+        ratio=torch.exp(next_neglogp-neglogp)
+        self.ratio=ratio
+        self.advantages_train=advantages_train
+        surr1=ratio*advantages_train
+        surr2=torch.clamp(ratio,1.0-self.clip_eps,1.0+self.clip_eps)*advantages_train
+        policy_loss=-torch.min(surr1,surr2)
+        #sum over sequence dim, mean over batch dim
+        policy_loss=torch.sum(policy_loss*mask_length,dim=0).mean()
+
+        value_clipped=values_train+torch.clamp(new_values_trains-values_train,-self.clip_eps,self.clip_eps)
+        surr1=(new_values_trains-values_train-advantages_train+baseline).pow(2)
+        surr2=(value_clipped-values_train-advantages_train+baseline).pow(2)
+        #surr1=(new_values_trains-values_train-advantages_train).pow(2)
+        #surr2=(value_clipped-values_train-advantages_train).pow(2)
+        value_loss=torch.max(surr1,surr2)
+        #sum over sequence dim, mean over batch dim
+        value_loss=torch.sum(value_loss*mask_length,dim=0).mean()
+
+        loss=policy_loss+value_loss*self.value_loss_coef-entropy*self.entropy_coef
+        mean_loss=loss.mean()
+        return mean_loss
+
