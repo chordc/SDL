e0c15ac87e6e06320b5c1f027785ee13fe41eea4
chordc <90882617+chordc@users.noreply.github.com>
2023-04-18 20:02:50 +0800

    Update learnppo.py

--- Projects/learnppo.py
+++ Projects/learnppo.py
@@ -98,7 +98,7 @@ def learner_ppo ( model,
             ppo1.optimizer.zero_grad()
             loss_val  .backward()
             #grad_norm = sum(p.grad.data.norm(2).item() ** 2 for p in ppo1.acmodel.parameters()) ** 0.5
-            #torch.nn.utils.clip_grad_norm_(ppo1.acmodel.parameters(), ppo1.max_grad_norm)
+            torch.nn.utils.clip_grad_norm_(ppo1.acmodel.parameters(), ppo1.max_grad_norm)
             ppo1.optimizer .step()
 
         # -------------------------------------------------
